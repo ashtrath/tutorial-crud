354e06fd1c5beb81ade0ce7f7352180285d70ff3
Rahadian Fahri Faizani <69681505+ashtrath@users.noreply.github.com>
2024-12-08 18:39:50 +0700

    fix: vite build error

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
                 'resources/css/app.css',
+                'resources/css/simplefolio/main.css',
+                'resources/css/simplefolio/media.css',
                 'resources/js/app.js',
             ],
             refresh: true,
